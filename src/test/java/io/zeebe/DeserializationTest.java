89a2b724c97e75d86ce5cc389c16201e5111b383
Alexey Vinogradov <vinogradov.a.i.93@gmail.com>
2022-05-16 14:13:25 +0700

    Let's verify that in the 8.0.2 there is no such problem.

--- src/test/java/io/zeebe/DeserializationTest.java
+++ src/test/java/io/zeebe/DeserializationTest.java
@@ -4,7 +4,8 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import io.camunda.zeebe.protocol.jackson.record.AbstractRecord;
+import io.camunda.zeebe.protocol.jackson.ZeebeProtocolModule;
+import io.camunda.zeebe.protocol.record.Record;
 import io.camunda.zeebe.protocol.record.value.JobRecordValue;
 import java.io.IOException;
 import java.io.InputStream;
@@ -17,16 +18,16 @@ public class DeserializationTest {
 
   @Before
   public void setUp() {
-    objectMapper = new ObjectMapper();
+    objectMapper = new ObjectMapper().registerModule(new ZeebeProtocolModule());
   }
 
   @Test
   public void shouldDeserializeJobRecordWithNullableVariables() throws IOException {
-    final AbstractRecord<JobRecordValue> record;
+    final Record<JobRecordValue> record;
     try (InputStream json = DeserializationTest.class.getResourceAsStream("/job-with-nullable-variable.json")) {
       record = objectMapper.readValue(json, new TypeReference<>() {});
     }
 
-    assertThat(record.getValue().getVariables()).size().isEqualTo(1);
+    assertThat(record.getValue().getVariables()).size().isEqualTo(2);
   }
 }
